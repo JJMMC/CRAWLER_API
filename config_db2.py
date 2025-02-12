0f88e6aebe099494b84797bb269089c0f9b3b9f2
JJMMC <jjmmc@hotmail.com>
2025-02-12 12:16:26 +0100

    Minor

--- config_db2.py
+++ config_db2.py
@@ -1,11 +1,11 @@
 import sqlite3 as sql
-import datetime
+import time
 from scrap_url import get_items_data, request_categorias_and_main_urls, find_child_urls
 
-path = "database/rtr_crawler_db2.db"
+path = "database/rtr_crawler.db"
 
 
-def create_tables ():
+def create_tables (path = "database/rtr_crawler.db"):
     with sql.connect(path) as connection:
         cursor = connection.cursor()
 
@@ -35,20 +35,13 @@ def create_tables ():
         connection.commit()
         print("Tabla -articulos- creada correctamente")
 
-def data_from_old_db(path = 'database/rtr_crawler_db.db'):
-    with sql.connect(path) as connection:
-        cursor = connection.cursor()
-        cursor.execute('''SELECT categoria, articulo, precio, fecha FROM precios ''')
-        old_db = cursor.fetchall()        
-    return old_db
-
-def obtener_articulo_id():
+# Función para obtener list de tuplas (id,nombre)
+def obtener_articulo_id_nombre(path = "database/rtr_crawler.db"):
     with sql.connect(path) as connection:
         cursor = connection.cursor()
         cursor.execute('''SELECT id, nombre FROM articulos ''')
         id_name_lst = cursor.fetchall()        
     return id_name_lst
-
 
 # Función para agregar un artículo
 def agregar_articulo(nombre, categoria):
@@ -59,60 +52,54 @@ def agregar_articulo(nombre, categoria):
         cursor.execute(instruction, (nombre, categoria))
         connection.commit()
 
-def transfer_db_articles():
-    old_db = data_from_old_db() #List tuplas
-    #id_nom_lst = obtener_articulo_id() #List tuplas
-    #nom_lst = [nom for id, nom in id_nom_lst] #Desempaquetamos y creamos lista de nombres unicos
-    for cat, nom, pre, fecha in old_db:
-        nom_lst = [nom for id, nom in obtener_articulo_id()] #Desempaquetamos y creamos lista de nombres unicos
-        if nom not in nom_lst:
-            agregar_articulo(nom,cat)
-            print("El artículo no está en la lista, agregamos artículo")
-        else:
-            print("el artículo SI está en la lista")
-            continue
-
 # Función para agregar el precio de un artículo al historial
-def agregar_precio(articulo_id, precio, fecha):
+def agregar_precio(articulo_id, precio, path = "database/rtr_crawler.db"):
     with sql.connect(path) as connection:
         cursor = connection.cursor()
-        instruction = '''INSERT INTO historial_precios (articulo_id, precio, fecha) 
-                      VALUES (?, ?, ?)'''
-        cursor.execute(instruction, (articulo_id, precio, fecha))
+        instruction = '''INSERT INTO historial_precios (articulo_id, precio) 
+                      VALUES (?, ?)'''
+        cursor.execute(instruction, (articulo_id, precio))
         connection.commit()
 
-def borrar_datos_tabla(tabla='historial_precios'):
-    with sql.connect(path) as connection:
-        cursor = connection.cursor()
-        instruction = f"DELETE FROM {tabla}"
-        cursor.execute(instruction)
-        connection.commit()
-        cursor.execute("VACUUM")
-        print(f"Todos los datos de la tabla {tabla} han sido borrados y el contador AUTOINCREMENT ha sido reiniciado.")
+
+def request_and_insert_all():
+    categories_and_urls = [(i[2],i[1]) for i in request_categorias_and_main_urls()]
+    for main_urls in categories_and_urls:         
+        child_urls = find_child_urls(main_urls[1]) #List    
+        for url in child_urls:
+                print (url)
+                item_list = get_items_data(url) #list(cat, name, price)
+                for cat_to_import, nom_to_import, pre_to_import in item_list:
+                    check_article_id_and_insert(cat_to_import, nom_to_import, pre_to_import)
+                
+
+def check_article_id_and_insert(cat_to_import, nom_to_import, pre_to_import):        
+    id_nom_lst = obtener_articulo_id_nombre()#lista tuplas(id,nombre) TABLA ARTICULOS
+
+    if nom_to_import in [nom for id, nom in id_nom_lst]:#Comprobamos si el artículo está en la TABLA ARTICULOS
+        print ("SI está en Tabla artículos")
+        for articulo_id, nombre in id_nom_lst: #Desempaquetamos list id's y nombres y nos movemos por la lista 
+            if nom_to_import == nombre: #Buscamos el id vía el nombre 
+                    print('ID localizado; Insertamos nuevo precio')
+                    agregar_precio(articulo_id, pre_to_import)
+                    #time.sleep(1)
+            else:
+                continue
+    else:
+        agregar_articulo(nom_to_import,cat_to_import)
+        check_article_id_and_insert(cat_to_import, nom_to_import, pre_to_import)
 
 
-def check_article_id_and_import(new_cat, new_nom, new_pre, new_fech):
-    id_nom_lst = obtener_articulo_id()
-    for articulo_id, nombre in id_nom_lst:
-        if new_nom == nombre:
-            print(f'El artículo:{new_nom} ya existe, su id de artículo es: {articulo_id}')
-            print(f'guardamos precio y fecha')
-            agregar_precio(articulo_id, new_pre, new_fech)
-        else:
-            continue
 
-def eliminar_tabla(tabla='historial_precios'):
-    with sql.connect(path) as connection:
-        cursor = connection.cursor()
-        instruction = f"DROP TABLE IF EXISTS {tabla}"
-        cursor.execute(instruction)
-        connection.commit()
-        print(f"La tabla {tabla} ha sido eliminada.")
+
+#request_and_insert_all()
 
 
 
-for cat, nomb, precio, fecha in data_from_old_db():
-    print(f'El item {nomb}, con precio {precio}')
-    check_article_id_and_import(cat, nomb, precio, fecha)
+
+
+
+
+
 
 
